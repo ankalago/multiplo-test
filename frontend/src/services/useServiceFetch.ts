2270051acc4f2b1a759f356581dfe5cfa762cde4
paul jacome <paul@minteo.com>
2025-01-31 19:28:56 +0100

    chore: add config files

--- frontend/src/services/useServiceFetch.ts
+++ frontend/src/services/useServiceFetch.ts
@@ -5,18 +5,20 @@ import useSWRMutation from 'swr/mutation'
 import { useEffect } from 'react'
 import { IMultigram } from '../entities/multigram'
 
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+
 export const useServiceFetch = <U extends IMultigram>() => {
 	const axiosInstance = AxiosServices.getInstance()
 	const dispatch = useDispatch()
 
 	const { data: getDataPost, trigger: triggerGetDataPost } = useSWRMutation([`api/v1/post`], (key) => {
-		return axiosInstance.get(`http://localhost:3000/${key}`)
+		return axiosInstance.get(`${BASE_API_URL}/${key}`)
 	})
 
 	const { data: updateDataPost, trigger: triggerUpdateDataPost } = useSWRMutation(
 		[`api/v1/post`],
 		(key: [string], { arg }: { arg: { id: string; like: boolean } }) => {
-			return axiosInstance.patch(`http://localhost:3000/${key}/${arg.id}`, { like: arg.like })
+			return axiosInstance.patch(`${BASE_API_URL}/${key}/${arg.id}`, { like: arg.like })
 		},
 		{
 			revalidate: true,
@@ -26,7 +28,7 @@ export const useServiceFetch = <U extends IMultigram>() => {
 	const { data: createDataPost, trigger: triggerCreateDataPost } = useSWRMutation(
 		[`api/v1/post`],
 		(key: [string], { arg }: { arg: { data: IMultigram } }) => {
-			return axiosInstance.post(`http://localhost:3000/${key}`, arg.data)
+			return axiosInstance.post(`${BASE_API_URL}/${key}`, arg.data)
 		},
 		{
 			revalidate: true,
